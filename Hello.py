e5d905a67d4af072cb4f1d9fe13e15f4b631d9a7
jy0604 <143371326+jy0604@users.noreply.github.com>
2023-09-28 15:01:30 -0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,7 +1,39 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
+# Load your CSV data
 df = pd.read_csv('toy_dataset.csv')
-df.head()
+
+# Group the data
+gen_med = df.groupby(['City', 'Gender'])['Income'].mean().reset_index(name='count')
+
+# Create a Streamlit app
+st.title("Average Income Per City by Gender")
+
+# Select a gender to display (using a sidebar)
+selected_gender = st.sidebar.selectbox("Select Gender", gen_med['Gender'].unique())
+
+# Filter data for the selected gender
+filtered_data = gen_med[gen_med['Gender'] == selected_gender]
+
+# Create a bar plot
+fig, ax = plt.subplots(figsize=(10, 5))
+x_pos = np.arange(len(filtered_data))
+tick_labels = filtered_data['City']
+
+# Plot the data
+ax.bar(x_pos, filtered_data['count'], width=0.4, label=selected_gender)
+ax.set_xticks(x_pos)
+ax.set_xticklabels(tick_labels, rotation=45, horizontalalignment='right')
+
+# Customize the plot
+ax.set_xlabel("City")
+ax.set_ylabel("Average Income")
+ax.set_title(f"Average Income for {selected_gender} in Each City")
+ax.legend()
+
+# Display the plot in Streamlit
+st.pyplot(fig)
 
