f5d3681ee34b1c43161e093e0199c0f6e1544436
jy0604 <143371326+jy0604@users.noreply.github.com>
2023-10-12 11:34:43 -0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -10,7 +10,7 @@ df = pd.read_csv('toy_dataset.csv')
 gen_med = df.groupby(['City', 'Gender'])['Income'].mean().reset_index(name='count')
 
 # Create a Streamlit app
-st.title("Average Income Per City by Gender")
+st.title("Some interesting data about toy")
 
 # Select a gender to display (using a sidebar)
 selected_gender = st.sidebar.selectbox("Select Gender", gen_med['Gender'].unique())
@@ -19,6 +19,8 @@ selected_gender = st.sidebar.selectbox("Select Gender", gen_med['Gender'].unique
 filtered_data = gen_med[gen_med['Gender'] == selected_gender]
 
 # Create a bar plot for the selected gender
+st.write(f"### Average Income for {selected_gender} in Each City")
+
 fig, ax = plt.subplots(figsize=(10, 5))
 x_pos = np.arange(len(filtered_data))
 tick_labels = filtered_data['City']
@@ -42,8 +44,10 @@ other_gender = 'Female' if selected_gender == 'Male' else 'Male'
 other_data = gen_med[gen_med['Gender'] == other_gender]
 
 # Create a separate bar plot for the other gender
+st.write(f"### Average Income for {other_gender} in Each City")
+
 fig2, ax2 = plt.subplots(figsize=(10, 5))
-x_pos2 = np.arange(len(other_data))
+x_pos2 = np.arange(len(other_data)
 
 # Plot the data
 ax2.bar(x_pos2, other_data['count'], width=0.4, label=other_gender, color='orange')
@@ -56,26 +60,13 @@ ax2.set_ylabel("Average Income")
 ax2.set_title(f"Average Income for {other_gender} in Each City")
 ax2.legend()
 
-fig = plt.figure()
-plt.pie(gen_med['count'], labels = gen_med['City'])
-
-# show plot
-plt.show()
-
 # Display the plot for the other gender in Streamlit
 st.pyplot(fig2)
 
-#comments
-st.write("There are some findings and a brief analysis ")
-st.write('1. The highest average income belongs to "Mountain View" and the lowest average income belongs to "Dallas"')
-st.write('2.Gender does not affect the overall trend in average income ')
-st.write('3.the average income of "Austin" is close to "Boston" ')
-
-
-
+# Pie chart
+st.write(f"### Pie Chart: Average Income Distribution in All Cities")
 fig = plt.figure()
-plt.pie(gen_med['count'], labels = gen_med['City'])
+plt.pie(gen_med['count'], labels=gen_med['City'], autopct='%1.1f%%', startangle=140)
 
-# show plot
-plt.show()
-
+# Display the pie chart in Streamlit
+st.pyplot(fig)
