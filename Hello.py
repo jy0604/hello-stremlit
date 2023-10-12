2ceb9c1e62f9a61f0f7d8301c24168c8717f41d9
jy0604 <143371326+jy0604@users.noreply.github.com>
2023-10-12 12:02:12 -0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -18,13 +18,13 @@ selected_gender = st.sidebar.selectbox("Select Gender", gen_med['Gender'].unique
 # Filter data for the selected gender
 filtered_data = gen_med[gen_med['Gender'] == selected_gender]
 
-# Create a bar plot for the selected gender
+# Create a bar plot for both genders combined
 fig, ax = plt.subplots(figsize=(10, 5))
 x_pos = np.arange(len(filtered_data))
 tick_labels = filtered_data['City']
 
 # Plot the data
-ax.bar(x_pos, filtered_data['count'], width=0.4, label=selected_gender)
+ax.bar(x_pos, filtered_data['count'], width=0.4, label=selected_gender, color='blue')
 ax.set_xticks(x_pos)
 ax.set_xticklabels(tick_labels, rotation=45, horizontalalignment='right')
 
@@ -37,30 +37,9 @@ ax.legend()
 # Display the plot in Streamlit
 st.pyplot(fig)
 
-# Filter data for the other gender
-other_gender = 'Female' if selected_gender == 'Male' else 'Male'
-other_data = gen_med[gen_med['Gender'] == other_gender]
-
-# Create a separate bar plot for the other gender
-fig2, ax2 = plt.subplots(figsize=(10, 5))
-x_pos2 = np.arange(len(other_data))
-
-# Plot the data
-ax2.bar(x_pos2, other_data['count'], width=0.4, label=other_gender, color='orange')
-ax2.set_xticks(x_pos2)
-ax2.set_xticklabels(other_data['City'], rotation=45, horizontalalignment='right')
-
-# Customize the plot
-ax2.set_xlabel("City")
-ax2.set_ylabel("Average Income")
-ax2.set_title(f"Average Income for {other_gender} in Each City")
-ax2.legend()
-
-# Display the plot for the other gender in Streamlit
+# Pie chart to show the distribution of data by gender
+gender_data = gen_med.groupby('Gender')['count'].sum()
+fig2 = plt.figure(figsize=(5, 5))
+plt.pie(gender_data, labels=gender_data.index, autopct='%1.1f%%')
+plt.title("Income Distribution by Gender")
 st.pyplot(fig2)
-
-# Pie chart to show the distribution of data by city
-fig3 = plt.figure(figsize=(10, 5))
-plt.pie(gen_med['count'], labels=gen_med['City'], autopct='%1.1f%%')
-plt.title("Income Distribution by City")
-st.pyplot(fig3)
