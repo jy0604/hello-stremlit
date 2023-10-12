28f89dedc215f303f63614e5483abc9e1f587cbe
jy0604 <143371326+jy0604@users.noreply.github.com>
2023-10-12 12:07:21 -0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -7,25 +7,33 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('toy_dataset.csv')
 
 # Group the data
-gen_med = df.groupby('Gender')['Income'].mean().reset_index(name='count')
+gen_med = df.groupby(['City', 'Gender'])['Income'].mean().reset_index(name='count')
 
 # Create a Streamlit app
-st.title("Average Income by Gender")
+st.title("Some interesting data about toy")
 
-# Create a bar plot for gender
-fig, ax = plt.subplots(figsize=(8, 6))
+# Create a bar plot for both genders combined
+fig, ax = plt.subplots(figsize=(10, 5))
 x_pos = np.arange(len(gen_med))
-tick_labels = gen_med['Gender']
+tick_labels = gen_med['City']
 
 # Plot the data
-ax.bar(x_pos, gen_med['count'], width=0.4, color='blue')
+ax.bar(x_pos, gen_med['count'], width=0.4, label='Both Genders', color='blue')
 ax.set_xticks(x_pos)
-ax.set_xticklabels(tick_labels)
+ax.set_xticklabels(tick_labels, rotation=45, horizontalalignment='right')
 
 # Customize the plot
-ax.set_xlabel("Gender")
+ax.set_xlabel("City")
 ax.set_ylabel("Average Income")
-ax.set_title("Average Income by Gender")
+ax.set_title("Average Income for Both Genders in Each City")
+ax.legend()
 
 # Display the plot in Streamlit
 st.pyplot(fig)
+
+# Pie chart to show the distribution of data by gender
+gender_data = gen_med.groupby('Gender')['count'].sum()
+fig2 = plt.figure(figsize=(5, 5))
+plt.pie(gender_data, labels=gender_data.index, autopct='%1.1f%%')
+plt.title("Income Distribution by Gender")
+st.pyplot(fig2)
